584d9369feff5ed31fba8b69f499fd8e03833cfe
Marat Akhmediev <akhmediev2003@mail.ru>
2024-03-06 00:15:31 +0500

    add black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 import uvicorn
-from pydantic import EmailStr, BaseModel
 
 from items_views import router as items_router
 from users.views import router as users_router
@@ -12,20 +11,15 @@ app.include_router(users_router)
 
 @app.get("/")
 def hello():
-    return [
-        "message like this"
-    ]
+    return ["message like this"]
 
 
 @app.get("/hello/")
 def hello_name(name: str, surname: str):
     name = name.title()
     surname = surname.title()
-    return {
-        "message": f"Hey, {name} {surname}"
-    }
+    return {"message": f"Hey, {name} {surname}"}
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
-
